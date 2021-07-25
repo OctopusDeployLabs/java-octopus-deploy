b1fa5bb25514cdf19f5c0147070a2d107bd34055
Trent Mohay <trent.mohay@octopus.com>
2021-07-26 08:55:16 +1000

    adding more types around spaces and projects to json

---
+++ tools/create_links_json_schema.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+API_KEY=API-D62EQ9I4EVET1E2LJUBKEHLNBYWMO3
+#ROOT_PATH='http://localhost:8065/api/'
+INPUT=$1
+
+createClassFromLinksBLock() {
+  CLASS_NAME=$1TypedLinks
+  shift
+  LINK_NAMES=`echo $1 | jq ' keys | .[]'`
+  writeClassHeader $CLASS_NAME
+  wrapStringInFunc $LINK_NAMES
+  echo "}"
+  echo ====================================
+}
+
+
+wrapStringInFunc() {
+  for i in $@
+  do
+    echo $i: {\"type\": \"string\"},
+  done
+}
+
+echo We are fetching $ROOT_PATH$INPUT
+CONTENT=`curl -s --header """X-Octopus-ApiKey: $API_KEY""" --header """Content-Type: application/json""" -X GET $ROOT_PATH$INPUT`
+
+LINKS_CONTENT=`echo $CONTENT | jq '.Links' | jq ' keys | .[]'`
+#LINKS_CONTENT=`cat | jq '.'`
+echo \"Links\":{
+echo \"type\": \"object\",
+echo \"properties\": {
+wrapStringInFunc $LINKS_CONTENT
+echo }
+echo }
+
+#SUB_LINKS=`echo $CONTENT | jq '.Items[0].Links'`
+#SUB_LINK_CLASS_NAME=${INPUT}Item
+#createClassFromLinksBLock "$SUB_LINK_CLASS_NAME" "$SUB_LINKS"

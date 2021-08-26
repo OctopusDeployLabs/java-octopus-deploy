2c94799c59370f1527781769d3a1da1b16cec869
Sean Long <89003774+long-sean@users.noreply.github.com>
2021-08-27 09:06:17 +1000

    Refactor for NPE and add test coverage (#8)
     - Update naming to clarify public method
     - Protect fromPathWithQueryString against NPE
     - Protect fromPathWithQueryString against valid paths with no params
     - Add test coverage

--- octopus-sdk/src/test/java/com/octopus/sdk/http/RequestEndpointTest.java
+++ octopus-sdk/src/test/java/com/octopus/sdk/http/RequestEndpointTest.java
@@ -17,6 +17,7 @@ package com.octopus.sdk.http;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.AbstractMap;
 
@@ -27,11 +28,41 @@ class RequestEndpointTest {
   @Test
   public void encodedPathDecodesIntoCorrectPathAndQueryItems() {
     final String input = "/api/path?keyOne=1&keyTwo=9";
-    final RequestEndpoint endPoint = RequestEndpoint.fromPathAndQueryString(input);
+    final RequestEndpoint endPoint = RequestEndpoint.fromPathWithQueryString(input);
     assertThat(endPoint.getPath()).isEqualTo("/api/path");
     assertThat(endPoint.getQueryParameters())
         .contains(
             new AbstractMap.SimpleEntry<>("keyOne", singletonList("1")),
             new AbstractMap.SimpleEntry<>("keyTwo", singletonList("9")));
+  }
+
+  @Test
+  public void invalidPathWithQueryThrowsIllegalArgumentException() {
+    final String input = "/api/inva?=&>lid"; // contains required and invalid chars
+    assertThatThrownBy(() -> RequestEndpoint.fromPathWithQueryString(input))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Unable to construct a RequestEndpoint from '" + input + "'");
+  }
+
+  @Test
+  public void nullPathWithQueryThrowsIllegalArgumentException() {
+    assertThatThrownBy(() -> RequestEndpoint.fromPathWithQueryString(null))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Unable to construct a RequestEndpoint from 'null'");
+  }
+
+  @Test
+  public void emptyPathWithQueryThrowsIllegalArgumentException() {
+    assertThatThrownBy(() -> RequestEndpoint.fromPathWithQueryString(""))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Unable to construct a RequestEndpoint from ''");
+  }
+
+  @Test
+  public void validPathWithNoQueryThrowsIllegalArgumentException() {
+    final String input = "/api/invalid";
+    assertThatThrownBy(() -> RequestEndpoint.fromPathWithQueryString(input))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Unable to construct a RequestEndpoint from '" + input + "'");
   }
 }

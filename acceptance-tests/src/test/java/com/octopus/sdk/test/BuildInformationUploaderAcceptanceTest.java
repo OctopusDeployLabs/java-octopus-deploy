fe6f7c997a7df697d8515e85ab08d7c4a9c3e4c2
Trent Mohay <trent.mohay@octopus.com>
2021-08-27 15:55:20 +1000

    clean up

--- acceptance-tests/src/test/java/com/octopus/sdk/test/BuildInformationUploaderAcceptanceTest.java
+++ acceptance-tests/src/test/java/com/octopus/sdk/test/BuildInformationUploaderAcceptanceTest.java
@@ -40,7 +40,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BuildInformationUploaderAcceptanceTest extends BaseAcceptanceTest {
+public class BuildInformationAcceptanceTest extends BaseAcceptanceTest {
 
   private SpacesOverviewApi spacesOverviewApi;
   private OctopusClient client;
--- octopus-sdk/src/main/java/com/octopus/sdk/operations/buildinformation/BuildInformationUploaderContext.java
+++ octopus-sdk/src/main/java/com/octopus/sdk/operations/buildinformation/BuildInformationUploaderContext.java
@@ -2,7 +2,7 @@
  * Copyright (c) Octopus Deploy and contributors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
- *  these files except in compliance with the License. You may obtain a copy of the
+ * these files except in compliance with the License. You may obtain a copy of the
  * License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0

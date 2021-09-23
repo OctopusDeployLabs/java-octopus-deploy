efc6fa60926e5e8105a8dff8f27c3b37aac2cb00
Sean Long <sean.long@octopus.com>
2021-09-23 13:49:09 +1000

    Add an appender handler - Methods for adding an appender to capture sdk logging - Expose log4j core dependency at compile time - Fix space logging output

---
+++ octopus-sdk/src/main/java/com/octopus/sdk/logging/AppenderHandler.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Octopus Deploy and contributors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ *  these files except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.octopus.sdk.logging;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+
+public class AppenderHandler {
+
+  private static final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+  private static final Configuration config = ctx.getConfiguration();
+
+  public static void registerLogAppender(Appender appender, Level sdkLogLevel) {
+    config.getRootLogger().addAppender(appender, sdkLogLevel, null);
+    config.getLoggerConfig("com.octopus").setLevel(sdkLogLevel);
+    ctx.updateLoggers();
+  }
+
+  public static void deregisterLogAppender(String appenderName) {
+    config.getRootLogger().removeAppender(appenderName);
+    config.getLoggerConfig("com.octopus").setLevel(Level.INFO);
+  }
+}

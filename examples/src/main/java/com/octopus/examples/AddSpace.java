ec200cf63b746b3204a414e3de12771b76f12d5a
Trent Mohay <trent.mohay@octopus.com>
2021-10-19 16:52:16 +1100

    addeding more examples

--- examples/src/main/java/com/octopus/examples/AddSpace.java
+++ examples/src/main/java/com/octopus/examples/AddSpace.java
@@ -16,21 +16,19 @@
 package com.octopus.examples;
 
 import com.octopus.sdk.Repository;
-import com.octopus.sdk.domain.Project;
-import com.octopus.sdk.domain.Release;
 import com.octopus.sdk.domain.Space;
 import com.octopus.sdk.http.ConnectData;
 import com.octopus.sdk.http.OctopusClient;
 import com.octopus.sdk.http.OctopusClientFactory;
-import com.octopus.sdk.model.release.ReleaseResource;
+import com.octopus.sdk.model.space.SpaceOverviewResource;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.Optional;
+import java.util.Collections;
 
-public class CreateReleaseWithVersion {
+public class AddSpace {
 
   final static String octopusServerUrl = "http://localhost:8065";
   final static String apiKey = "YOUR_API_KEY"; // as read from your profile in your Octopus Deploy server
@@ -39,20 +37,9 @@ public class CreateReleaseWithVersion {
     final OctopusClient client = createClient();
 
     final Repository repo = new Repository(client);
-    final Optional<Space> space = repo.spaces().getByName("TheSpaceName");
-    if(!space.isPresent()) {
-      System.out.println("No space named 'TheSpaceName' exists on server");
-      return;
-    }
+    final Space createdSpace = repo.spaces().create(new SpaceOverviewResource("NewSpaceName", Collections.singleton(
+        "spaceManagerTeamMembers")));
 
-    final Optional<Project> project = space.get().projects().getByName("TheProjectName");
-    if(!project.isPresent()) {
-      System.out.println("No project named 'TheProjectName' exists on server");
-      return;
-    }
-
-    final ReleaseResource releaseResource = new ReleaseResource("1.0", project.get().getProperties().getId());
-    final Release release = space.get().releases().create(releaseResource);
   }
 
   // Create an authenticated connection to your Octopus Deploy Server
--- examples/src/main/java/com/octopus/examples/DeleteSpace.java
+++ examples/src/main/java/com/octopus/examples/DeleteSpace.java
@@ -16,13 +16,10 @@
 package com.octopus.examples;
 
 import com.octopus.sdk.Repository;
-import com.octopus.sdk.domain.Project;
-import com.octopus.sdk.domain.Release;
 import com.octopus.sdk.domain.Space;
 import com.octopus.sdk.http.ConnectData;
 import com.octopus.sdk.http.OctopusClient;
 import com.octopus.sdk.http.OctopusClientFactory;
-import com.octopus.sdk.model.release.ReleaseResource;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -30,14 +27,13 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.Optional;
 
-public class CreateReleaseWithVersion {
+public class DeleteSpace {
 
   final static String octopusServerUrl = "http://localhost:8065";
   final static String apiKey = "YOUR_API_KEY"; // as read from your profile in your Octopus Deploy server
 
   public static void main(final String... args) throws IOException {
     final OctopusClient client = createClient();
-
     final Repository repo = new Repository(client);
     final Optional<Space> space = repo.spaces().getByName("TheSpaceName");
     if(!space.isPresent()) {
@@ -45,14 +41,9 @@ public class CreateReleaseWithVersion {
       return;
     }
 
-    final Optional<Project> project = space.get().projects().getByName("TheProjectName");
-    if(!project.isPresent()) {
-      System.out.println("No project named 'TheProjectName' exists on server");
-      return;
-    }
-
-    final ReleaseResource releaseResource = new ReleaseResource("1.0", project.get().getProperties().getId());
-    final Release release = space.get().releases().create(releaseResource);
+    space.get().getProperties().setTaskQueueStopped(true);
+    final Space stoppedSpace = repo.spaces().update(space.get().getProperties());
+    repo.spaces().delete(stoppedSpace.getProperties());
   }
 
   // Create an authenticated connection to your Octopus Deploy Server

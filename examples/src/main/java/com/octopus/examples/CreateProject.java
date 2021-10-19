785fe44d85771252db7231813a6d601dd3f4af01
Trent Mohay <trent.mohay@octopus.com>
2021-10-19 16:52:16 +1100

    addeding more examples

--- examples/src/main/java/com/octopus/examples/CreateProject.java
+++ examples/src/main/java/com/octopus/examples/CreateProject.java
@@ -14,12 +14,14 @@
  */
 
 package com.octopus.examples;import com.octopus.sdk.Repository;
+import com.octopus.sdk.domain.Lifecycle;
+import com.octopus.sdk.domain.Project;
 import com.octopus.sdk.domain.ProjectGroup;
 import com.octopus.sdk.domain.Space;
 import com.octopus.sdk.http.ConnectData;
 import com.octopus.sdk.http.OctopusClient;
 import com.octopus.sdk.http.OctopusClientFactory;
-import com.octopus.sdk.model.projectgroup.ProjectGroupResource;
+import com.octopus.sdk.model.project.ProjectResource;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -27,7 +29,7 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.Optional;
 
-public class CreateProjectGroupExample {
+public class CreateProject {
 
   final static String octopusServerUrl = "http://localhost:8065";
   final static String apiKey = "YOUR_API_KEY"; // as read from your profile in your Octopus Deploy server
@@ -42,8 +44,24 @@ public class CreateProjectGroupExample {
       System.out.println("No space named 'TheSpaceName' exists on server");
       return;
     }
-    final ProjectGroupResource projectGroupResource = new ProjectGroupResource("TheProjectGroup");
-    final ProjectGroup projectGroup = space.get().projectGroups().create(projectGroupResource);
+
+    final Optional<Lifecycle> lifecycle = space.get().lifecycles().getByName("MyLifecycle");
+    if(!lifecycle.isPresent()) {
+      System.out.println("No lifecycle named 'MyLifecycle' exists on server");
+      return;
+    }
+
+    final Optional<ProjectGroup> projGroup = space.get().projectGroups().getByName("MyProjectGroup");
+    if(!projGroup.isPresent()) {
+      System.out.println("No lifecycle named 'MyProjectGroup' exists on server");
+      return;
+    }
+
+    final ProjectResource projectToCreate = new ProjectResource("Test Project",
+        lifecycle.get().getProperties().getId(),
+        projGroup.get().getProperties().getId());
+    projectToCreate.setAutoCreateRelease(false);
+    final Project createdProject = space.get().projects().create(projectToCreate);
 
   }
 
--- examples/src/main/java/com/octopus/examples/CreateProjectGroup.java
+++ examples/src/main/java/com/octopus/examples/CreateProjectGroup.java
@@ -13,16 +13,13 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package com.octopus.examples;
-
-import com.octopus.sdk.Repository;
-import com.octopus.sdk.domain.Project;
-import com.octopus.sdk.domain.Release;
+package com.octopus.examples;import com.octopus.sdk.Repository;
+import com.octopus.sdk.domain.ProjectGroup;
 import com.octopus.sdk.domain.Space;
 import com.octopus.sdk.http.ConnectData;
 import com.octopus.sdk.http.OctopusClient;
 import com.octopus.sdk.http.OctopusClientFactory;
-import com.octopus.sdk.model.release.ReleaseResource;
+import com.octopus.sdk.model.projectgroup.ProjectGroupResource;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -30,7 +27,7 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.Optional;
 
-public class CreateReleaseWithVersion {
+public class CreateProjectGroup {
 
   final static String octopusServerUrl = "http://localhost:8065";
   final static String apiKey = "YOUR_API_KEY"; // as read from your profile in your Octopus Deploy server
@@ -40,19 +37,14 @@ public class CreateReleaseWithVersion {
 
     final Repository repo = new Repository(client);
     final Optional<Space> space = repo.spaces().getByName("TheSpaceName");
+
     if(!space.isPresent()) {
       System.out.println("No space named 'TheSpaceName' exists on server");
       return;
     }
+    final ProjectGroupResource projectGroupResource = new ProjectGroupResource("TheProjectGroup");
+    final ProjectGroup projectGroup = space.get().projectGroups().create(projectGroupResource);
 
-    final Optional<Project> project = space.get().projects().getByName("TheProjectName");
-    if(!project.isPresent()) {
-      System.out.println("No project named 'TheProjectName' exists on server");
-      return;
-    }
-
-    final ReleaseResource releaseResource = new ReleaseResource("1.0", project.get().getProperties().getId());
-    final Release release = space.get().releases().create(releaseResource);
   }
 
   // Create an authenticated connection to your Octopus Deploy Server
